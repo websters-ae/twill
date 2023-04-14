b988a7d70e6b6de9d0cf328bfe2dfee2761c91e9
celso.j@websters.ae <celso.j@websters.ae>
2023-04-14 15:55:18 +0400

    update

--- src/Models/Behaviors/HasRevisions.php
+++ src/Models/Behaviors/HasRevisions.php
@@ -55,12 +55,11 @@ trait HasRevisions
      */
     public function revisionsArray()
     {
-        return $this->revisions->map(function ($revision, $index) {
+        return $this->revisions->map(function ($revision) {
             return [
                 'id' => $revision->id,
                 'author' => $revision->user->name ?? 'Unknown',
                 'datetime' => $revision->created_at->toIso8601String(),
-                'label' => $index === 0 ? twillTrans('twill::lang.publisher.current') : '',
             ];
         })->toArray();
     }
